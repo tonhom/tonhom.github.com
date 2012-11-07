628e74fb33e560cadbcab8517d4d3849a4f57dcf
tonhombuu <tohom.buu@gmail.com>
2012-11-07 20:37:01 +0700

    update core

--- appbars/rs/core.js
+++ appbars/rs/core.js
@@ -2,7 +2,8 @@ this.window.handler = {
     add:function(){
         $("div.appbar > div").click(function(){
             var func = $(this).attr("handler");
-            func+"();";
+            func+="();";
+            console.log(func);
             eval(func.toLowerCase());
             $.fn.appbar.close();
         });
